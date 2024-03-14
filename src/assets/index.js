ad2733818dca363c38347a249dd06099ac8711a3
Muhammad Afrasyab <makust7@gmail.com>
2024-03-14 23:25:46 +0500

    ReImport the images of star and hide

--- src/assets/index.js
+++ src/assets/index.js
@@ -20,8 +20,10 @@ import prepare from "./images/prepare.png"
 import range from "./images/range.png";
 import next_icon from "./images/next_arrow.png"
 import mob_icon from "./images/mobIcon.png"
+// import hide from "./images/UX/Icon/Action/hide.png"
+// import star from "./images/UX/Icon/Action/star.png"
 import hide from "./images/UX/Icon/Action/hide.png"
-import star from "./images/UX/Icon/Action/star.png"
+import star from "./images/UX/Icon/Action/star.png";
 import starred from "./images/UX/Icon/Action/starred.png"
 import revenue from "./images/revenue.png";
 import profit from "./images/profit.png";
