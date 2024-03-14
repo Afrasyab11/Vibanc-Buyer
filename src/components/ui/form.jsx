953ad65968fefa52e3cf6eb90fc650454aa4c668
Muhammad Afrasyab <makust7@gmail.com>
2024-03-15 00:37:37 +0500

    comment the FormItemContext

--- src/components/ui/form.jsx
+++ src/components/ui/form.jsx
@@ -50,9 +50,9 @@ const FormItem = React.forwardRef(({ className, ...props }, ref) => {
   const id = React.useId()
 
   return (
-    (<FormItemContext.Provider value={{ id }}>
+    // (<FormItemContext.Provider value={{ id }}>
       <div ref={ref} className={cn("space-y-2", className)} {...props} />
-    </FormItemContext.Provider>)
+    // </FormItemContext.Provider>)
   );
 })
 FormItem.displayName = "FormItem"
